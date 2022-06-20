f1ef46036e60500860e8c5649650f1383332ebb4
Diego Tacconi <diego@tacconi.org>
2022-06-19 20:57:46 -0500

    Add preliminary code to try to parse the barcode label.

---
+++ Steps/TapExtensions.Steps/BarcodeScanner/ParseRawBytes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using OpenTap;
+
+namespace TapExtensions.Steps.BarcodeScanner
+{
+    [Display("ParseRawBytes",
+        Groups: new[] { "TapExtensions", "Steps", "BarcodeScanner" })]
+    public class ParseRawBytes : TestStep
+    {
+        public override void Run()
+        {
+            try
+            {
+                // Example parsing the barcode row bytes
+
+
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+            }
+        }
+    }
+}
