5d7c3a2859297240af7bc011d55f19349a171d7a
Diego Tacconi <diego@tacconi.org>
2022-08-21 15:54:26 -0500

    Add test step to get the barcode label.

---
+++ Steps/TapExtensions.Steps/BarcodeScanner/GetBarcodeLabel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpenTap;
+using TapExtensions.Interfaces.BarcodeScanner;
+
+namespace TapExtensions.Steps.BarcodeScanner
+{
+    [Display("GetBarcodeLabel",
+        Groups: new[] { "TapExtensions", "Steps", "BarcodeScanner" })]
+    public class GetBarcodeLabel : TestStep
+    {
+        [Display("BarcodeScanner")]
+        public IBarcodeScanner BarcodeScanner { get; set; }
+
+        public override void Run()
+        {
+            try
+            {
+                var (serialNumber, productCode) = BarcodeScanner.GetBarcodeLabel();
+                Log.Debug($"productCode  = '{productCode}'");
+                Log.Debug($"serialNumber = '{serialNumber}'");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                // Publish(Name, false, true, true, "bool");
+            }
+        }
+    }
+}
