fe1daa153ef25d3cd093500ed5f90bd00bee0524
Diego Tacconi <diego@tacconi.org>
2023-06-09 20:05:46 -0500

    TestingBarcodeLabelUtility

---
+++ Steps/TapExtensions.Steps/BarcodeScanner/BarcodeLabelUtility.cs
@@ -0,0 +1,159 @@
+﻿// Methods for parsing barcode labels using the data structure syntax of ISO 15434 and ISO 15418 standards
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TapExtensions.Steps.BarcodeScanner
+{
+    public class BarcodeLabelUtility
+    {
+        public static string GetProductCode(byte[] rawBytes)
+        {
+            return SectionToString(rawBytes, EHeader.ProductCode);
+        }
+
+        public static string GetSerialNumber(byte[] rawBytes)
+        {
+            return SectionToString(rawBytes, EHeader.SerialNumber);
+        }
+
+        private enum EHeader
+        {
+            ProductCode,     // 1P
+            SerialNumber,    // S
+            Company,         // 18V
+            Date,            // 10D
+            Traceability,    // 1T
+            CountryOfOrigin, // 4L
+            Quantity         // Q
+        }
+
+        private static byte[] GetHeaderIdentifier(EHeader header)
+        {
+            byte[] identifier;
+            switch (header)
+            {
+                case EHeader.ProductCode:
+                    identifier = new byte[] { 0x31, 0x50 }; // 1P = Manufacturer Product Code
+                    break;
+                case EHeader.SerialNumber:
+                    identifier = new byte[] { 0x53 }; // S = Serial Number
+                    break;
+                case EHeader.Company:
+                    identifier = new byte[] { 0x31, 0x38, 0x56 }; // 18V = Company Identification Number (CIN)
+                    break;
+                case EHeader.Date:
+                    identifier = new byte[] { 0x31, 0x30, 0x44 }; // 10D = Date (Format YYWW)
+                    break;
+                case EHeader.Traceability:
+                    identifier = new byte[] { 0x31, 0x54 }; // 1T = Traceability Number (Lot/Batch Number)
+                    break;
+                case EHeader.CountryOfOrigin:
+                    identifier = new byte[] { 0x34, 0x4c }; // 4L = Country of Origin, two-character ISO 3166 country code
+                    break;
+                case EHeader.Quantity:
+                    identifier = new byte[] { 0x51 }; // Q = Quantity, Number of Pieces, or Amount (numeric only)
+                    break;
+                default:
+                    throw new ArgumentException(
+                        $"Case not found for {nameof(header)}={header}");
+            }
+
+            return identifier;
+        }
+
+        private static string SectionToString(byte[] rawBytes, EHeader header)
+        {
+            if (rawBytes == null || rawBytes.Length == 0)
+                throw new InvalidOperationException($"{nameof(rawBytes)} is null or empty");
+
+            var headerIdentifier = GetHeaderIdentifier(header);
+            var sectionBytes = GetSection(rawBytes, headerIdentifier);
+            var section = AsciiBytesToString(sectionBytes);
+            return section;
+        }
+
+        private static string AsciiBytesToString(byte[] bytes)
+        {
+            var msg = new StringBuilder();
+            if (bytes != null && bytes.Length != 0)
+            {
+                foreach (var b in bytes)
+                {
+                    if (b >= 0x20 && b <= 0x7E)
+                        msg.Append((char)b);
+                    else
+                        msg.Append("{" + b.ToString("X2") + "}");
+                }
+            }
+            return msg.ToString();
+        }
+
+        private static byte[] GetSection(byte[] source, byte[] header)
+        {
+            var gsBytes = new byte[] { 0x5B, 0x47, 0x53, 0x5D }; // Group Separator (as an array or ASCII characters) [GS]
+            var rsBytes = new byte[] { 0x5B, 0x52, 0x53, 0x5D }; // Record Separator (as an array or ASCII characters) [RS]
+            var eofBytes = new byte[] { 0x5B, 0x45, 0x4F, 0x54, 0x5D }; // End of Transmission (as an array or ASCII characters) [EOT]
+
+            const byte gs = 0x1D; // Group Separator (as a single byte)
+            const byte rs = 0x1E; // Record Separator (as a single byte)
+            const byte eof = 0x04; // End of Transmission (as a single byte)
+            var delimiters = new List<byte> { gs, rs, eof };
+            var sections = Split(source, delimiters);
+
+            foreach (var section in sections)
+            {
+                var beginningSection = section.Take(header.Length).ToArray();
+                if (beginningSection.SequenceEqual(header))
+                {
+                    var sectionWithoutHeader = new byte[section.Length - header.Length];
+                    Array.Copy(section, header.Length, sectionWithoutHeader, 0, section.Length - header.Length);
+                    return sectionWithoutHeader;
+                }
+            }
+
+            throw new InvalidOperationException(
+                $"Cannot find section with header of '{AsciiBytesToString(header)}'");
+        }
+
+        private static List<byte[]> Split(byte[] source, List<byte> delimiters)
+        {
+            var parts = new List<byte[]>();
+            var startIndex = 0;
+            byte[] part;
+
+            // Find delimiters
+            for (var i = 0; i < source.Length; i++)
+            {
+                if (Equals(source[i], delimiters))
+                {
+                    part = new byte[i - startIndex];
+                    Array.Copy(source, startIndex, part, 0, part.Length);
+                    parts.Add(part);
+                    startIndex = i + 1;
+                }
+            }
+
+            // Remaining part
+            part = new byte[source.Length - startIndex];
+            Array.Copy(source, startIndex, part, 0, part.Length);
+            parts.Add(part);
+
+            // Remove empty parts
+            parts.RemoveAll(b => b.Length == 0);
+
+            return parts;
+        }
+
+        private static bool Equals(byte source, List<byte> delimiters)
+        {
+            foreach (var delimiter in delimiters)
+                if (source == delimiter)
+                    return true;
+
+            return false;
+        }
+    }
+}
---
+++ Steps/TapExtensions.Steps/BarcodeScanner/TestingBarcodeLabelUtility.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using OpenTap;
+
+namespace TapExtensions.Steps.BarcodeScanner
+{
+    [Display("TestingBarcodeLabelUtility",
+    Groups: new[] { "TapExtensions", "Steps", "BarcodeScanner" })]
+    public class TestingBarcodeLabelUtility : TestStep
+    {
+        public override void Run()
+        {
+            try
+            {
+                // var barcodeString = "[)>[RS]06[GS]1P089659A.X12[GS]SL1194215398[GS]1T1RK6EUCJ[RS][EOT]";
+                // var rawBytes = Encoding.ASCII.GetBytes(barcodeString);
+
+                var rawBytes = new byte[] { 0x5B, 0x29, 0x3E, 0x1E, 0x30, 0x36, 0x1D, 0x31, 0x50, 0x30, 0x38, 0x39, 0x36, 0x35, 0x39, 0x41, 0x2E, 0x58, 0x31, 0x32, 0x1D, 0x53, 0x4C, 0x31, 0x31, 0x39, 0x34, 0x32, 0x31, 0x35, 0x33, 0x39, 0x38, 0x1D, 0x31, 0x54, 0x31, 0x52, 0x4B, 0x36, 0x45, 0x55, 0x43, 0x4A, 0x1E, 0x04 };
+
+                var hex = new StringBuilder();
+                var ascii = new StringBuilder();
+                foreach (var c in rawBytes)
+                {
+                    hex.Append(c.ToString("X2") + " ");
+
+                    var j = c;
+                    if (j >= 0x20 && j <= 0x7E)
+                        ascii.Append((char)j + "  ");
+                    else
+                        ascii.Append('.' + "  ");
+                }
+                Log.Debug($"Hex:   {hex}");
+                Log.Debug($"Ascii: {ascii}");
+
+
+
+
+                // Parse the barcode label
+                var productCode = BarcodeLabelUtility.GetProductCode(rawBytes);
+                var serialNumber = BarcodeLabelUtility.GetSerialNumber(rawBytes);
+
+                Log.Debug($"productCode  = '{productCode}'");
+                Log.Debug($"serialNumber = '{serialNumber}'");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+            }
+        }
+    }
+}
