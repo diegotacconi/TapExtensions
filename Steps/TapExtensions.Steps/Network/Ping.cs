3d1d08dc70632834c5d4409e46a82a0081bb580a
Diego Tacconi <diego.tacconi@nokia.com>
2024-02-22 13:56:17 -0600

    Change log messages from Warning to Debug

--- Steps/TapExtensions.Steps/Network/Ping.cs
+++ Steps/TapExtensions.Steps/Network/Ping.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Net;
 using System.Net.NetworkInformation;
@@ -14,8 +14,7 @@ namespace TapExtensions.Steps.Network
     {
         #region Settings
 
-        [Display("IP Address", Order: 1)]
-        public string IpAddress { get; set; }
+        [Display("IP Address", Order: 1)] public string IpAddress { get; set; }
 
         [Display("Min Ping Replies", Order: 2,
             Description: "Minimum number of successful ping replies required for this test to pass")]
@@ -46,10 +45,7 @@ namespace TapExtensions.Steps.Network
         {
             try
             {
-                var ipAddress = IPAddress.Parse(IpAddress);
-                var timeoutMs = Timeout * 1000;
-                var pingOk = PingHost(ipAddress, timeoutMs, MinSuccessfulReplies);
-
+                var pingOk = PingHost(IpAddress, Timeout, MinSuccessfulReplies);
                 // Publish(Name, pingOk, true, true, "bool");
             }
             catch (Exception ex)
@@ -59,40 +55,38 @@ namespace TapExtensions.Steps.Network
             }
         }
 
-        private bool PingHost(IPAddress ipAddress, int timeoutMs, int minSuccessfulReplies)
+        private bool PingHost(string ipAddress, int timeout, int minSuccessfulReplies)
         {
-            var keepOnPinging = true;
             var pingOk = false;
             var pingOkReplies = 0;
             var timer = new Stopwatch();
+            var timeoutMs = (long)timeout * 1000;
 
             using (var pingSender = new System.Net.NetworkInformation.Ping())
             {
                 // Create a buffer of 32 bytes of data to be transmitted.
                 var buffer = Encoding.ASCII.GetBytes("12345678901234567890123456789012");
 
-                Log.Info($"Pinging {ipAddress}");
+                var address = IPAddress.Parse(ipAddress);
+                Log.Info($"Pinging {address}");
                 timer.Start();
-                do
+
+                while (timer.ElapsedMilliseconds < timeoutMs)
                 {
                     // Use same timeout as in DOS prompt default, which is 4 seconds
-                    var reply = pingSender.Send(ipAddress, 4000, buffer);
+                    var reply = pingSender.Send(address, 4000, buffer);
                     if (reply != null && reply.Status == IPStatus.Success)
                     {
                         // Ping success
-                        var roundtripTime = reply.RoundtripTime < 1 ? "<1ms" : string.Format($"={reply.RoundtripTime}ms");
+                        var roundtripTime = reply.RoundtripTime < 1 ? "<1ms" : $"={reply.RoundtripTime}ms";
                         Log.Debug(
                             $"Ping reply from {reply.Address}: bytes={reply.Buffer.Length} time{roundtripTime} TTL={reply.Options.Ttl}");
 
                         pingOkReplies++;
                         if (pingOkReplies >= minSuccessfulReplies)
                         {
-                            keepOnPinging = false;
                             pingOk = true;
-                        }
-                        else
-                        {
-                            TapThread.Sleep(1000);
+                            break;
                         }
                     }
                     else
@@ -102,23 +96,19 @@ namespace TapExtensions.Steps.Network
                         {
                             // Convert camelCase to sentence with spaces
                             var status = Regex.Replace(reply.Status.ToString(), "([A-Z0-9]+)", " $1").ToLower().Trim();
-                            Log.Warning($"Ping request {status}.");
+                            Log.Debug($"Ping request {status} (ping failed).");
                         }
                         else
                         {
-                            Log.Warning("Ping request failed.");
+                            Log.Debug("Ping request failed.");
                         }
-
-                        if (timer.ElapsedMilliseconds > timeoutMs)
-                            keepOnPinging = false;
 
                         pingOkReplies = 0;
                     }
 
-                    // Offer GUI to break at this point
+                    TapThread.Sleep(TimeSpan.FromSeconds(1));
                     OfferBreak();
-
-                } while (keepOnPinging);
+                }
             }
 
             return pingOk;
