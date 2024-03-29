0343ab844758dd6106012d8a0297e5d056a23b98
diegotacconi <diego@tacconi.org>
2024-03-29 13:13:14 -0500

    Add steps to transfer a single file

---
+++ Steps/TapExtensions.Steps/Ssh/SshDownloadFile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OpenTap;
+using TapExtensions.Interfaces.Ssh;
+
+namespace TapExtensions.Steps.Ssh
+{
+    [Display("SshDownloadFile",
+        Groups: new[] { "TapExtensions", "Steps", "Ssh" })]
+    public class SshDownloadFile : TestStep
+    {
+        #region Settings
+
+        [Display("Dut", Order: 1)] public ISecureShell Dut { get; set; }
+
+        [Display("Remote File (Source)", Order: 2,
+            Description: "Full path of the remote file location")]
+        public string RemoteFile { get; set; }
+
+        [Display("Local File (Destination)", Order: 3,
+            Description: "Full path to the local file location")]
+        public string LocalFile { get; set; }
+
+        #endregion
+
+        public SshDownloadFile()
+        {
+            // Default values
+            RemoteFile = "/tmp/img100.jpg";
+            LocalFile = @"C:\Temp\img100.jpg";
+
+            // Validation rules
+            Rules.Add(() => !string.IsNullOrWhiteSpace(RemoteFile),
+                "Cannot be empty", nameof(RemoteFile));
+            Rules.Add(() => !string.IsNullOrWhiteSpace(LocalFile),
+                "Cannot be empty", nameof(LocalFile));
+            Rules.Add(() => RemoteFile?.IndexOfAny(Path.GetInvalidPathChars()) < 0,
+                "Not valid", nameof(RemoteFile));
+            Rules.Add(() => LocalFile?.IndexOfAny(Path.GetInvalidPathChars()) < 0,
+                "Not valid", nameof(LocalFile));
+        }
+
+        public override void Run()
+        {
+            try
+            {
+                var files = new List<(string, string)>
+                {
+                    (RemoteFile, LocalFile)
+                };
+
+                Dut.DownloadFiles(files);
+                UpgradeVerdict(Verdict.Pass);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                UpgradeVerdict(Verdict.Fail);
+            }
+        }
+    }
+}
---
+++ Steps/TapExtensions.Steps/Ssh/SshUploadFile.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OpenTap;
+using TapExtensions.Interfaces.Ssh;
+
+namespace TapExtensions.Steps.Ssh
+{
+    [Display("SshUploadFile",
+        Groups: new[] { "TapExtensions", "Steps", "Ssh" })]
+    public class SshUploadFile : TestStep
+    {
+        #region Settings
+
+        [Display("Dut", Order: 1)] public ISecureShell Dut { get; set; }
+
+        [Display("Local File (Source)", Order: 2,
+            Description: "Full path of the local file location")]
+        [FilePath]
+        public string LocalFile
+        {
+            get => _localFile;
+            set => _localFile = !string.IsNullOrWhiteSpace(value) ? Path.GetFullPath(value) : "";
+        }
+
+        private string _localFile;
+
+        [Display("Remote File (Destination)", Order: 3,
+            Description: "Full path to the remote file location")]
+        public string RemoteFile { get; set; }
+
+        #endregion
+
+        public SshUploadFile()
+        {
+            // Default values
+            LocalFile = @"C:\Windows\Web\Screen\img100.jpg";
+            RemoteFile = "/tmp/img100.jpg";
+
+            // Validation rules
+            Rules.Add(() => !string.IsNullOrWhiteSpace(LocalFile),
+                "Cannot be empty", nameof(LocalFile));
+            Rules.Add(() => !string.IsNullOrWhiteSpace(RemoteFile),
+                "Cannot be empty", nameof(RemoteFile));
+            Rules.Add(() => LocalFile?.IndexOfAny(Path.GetInvalidPathChars()) < 0,
+                "Not valid", nameof(LocalFile));
+            Rules.Add(() => RemoteFile?.IndexOfAny(Path.GetInvalidPathChars()) < 0,
+                "Not valid", nameof(RemoteFile));
+        }
+
+        public override void Run()
+        {
+            try
+            {
+                var files = new List<(string, string)>
+                {
+                    (LocalFile, RemoteFile)
+                };
+
+                Dut.UploadFiles(files);
+                UpgradeVerdict(Verdict.Pass);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                UpgradeVerdict(Verdict.Fail);
+            }
+        }
+    }
+}
