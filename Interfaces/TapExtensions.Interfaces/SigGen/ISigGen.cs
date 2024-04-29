609cb1983fe22e82edcbe3143a4ad4d1656e3c2f
diegotacconi <diego@tacconi.org>
2024-04-29 12:11:12 -0500

    Add Instruments.SigGen and preliminary SynthUsb2

---
+++ Interfaces/TapExtensions.Interfaces/SigGen/ISigGen.cs
@@ -0,0 +1,20 @@
+﻿using OpenTap;
+using TapExtensions.Interfaces.Common;
+
+namespace TapExtensions.Interfaces.SigGen
+{
+    public interface ISigGen : IInstrument
+    {
+        double GetFrequency();
+
+        double GetOutputLevel();
+
+        EState GetRfOutputState();
+
+        void SetFrequency(double frequencyMhz);
+
+        void SetOutputLevel(double outputLevelDbm);
+
+        void SetRfOutputState(EState state);
+    }
+}
