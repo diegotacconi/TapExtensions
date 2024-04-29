e30bff8a074c816bd52e5ba64114b83a3889fe5e
diegotacconi <diego@tacconi.org>
2024-04-29 12:19:01 -0500

    Add preliminary SynthUsb3

---
+++ Instruments/TapExtensions.Instruments.SigGen/SynthUsb3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using OpenTap;
+using TapExtensions.Interfaces.Common;
+using TapExtensions.Interfaces.SigGen;
+
+namespace TapExtensions.Instruments.SigGen
+{
+    public class SynthUsb3 : Instrument, ISigGen
+    {
+        public double GetFrequency()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetOutputLevel()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EState GetRfOutputState()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetFrequency(double frequencyMhz)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetOutputLevel(double outputLevelDbm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetRfOutputState(EState state)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
