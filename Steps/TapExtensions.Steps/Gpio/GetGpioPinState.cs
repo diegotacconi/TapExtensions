e3ae6ac4f17e491b55d86543e8b49bc9bea10834
Diego Tacconi <diego.tacconi@nokia.com>
2024-06-12 19:12:39 -0500

    Add basic steps to control GPIO pins

---
+++ Steps/TapExtensions.Steps/Gpio/GetGpioPinState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenTap;
+using TapExtensions.Interfaces.Gpio;
+
+namespace TapExtensions.Steps.Gpio
+{
+    [Display("GetGpioPinState", Groups: new[] { "TapExtensions", "Steps", "Gpio" })]
+    public class GetGpioPinState : TestStep
+    {
+        [Display("Gpio", Order: 1)] public IGpio Gpio { get; set; }
+
+        [Display("Pin Number", Order: 2)] public int PinNumber { get; set; }
+
+        [Display("Expected Pin State", Order: 3)]
+        public EPinState ExpectedPinState { get; set; }
+
+        public override void Run()
+        {
+            try
+            {
+                var measuredPinState = Gpio.GetPinState(PinNumber);
+                UpgradeVerdict(measuredPinState == ExpectedPinState ? Verdict.Pass : Verdict.Fail);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                UpgradeVerdict(Verdict.Fail);
+            }
+        }
+    }
+}
---
+++ Steps/TapExtensions.Steps/Gpio/SetGpioPinState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OpenTap;
+using TapExtensions.Interfaces.Gpio;
+
+namespace TapExtensions.Steps.Gpio
+{
+    [Display("SetGpioPinState", Groups: new[] { "TapExtensions", "Steps", "Gpio" })]
+    public class SetGpioPinState : TestStep
+    {
+        [Display("Gpio", Order: 1)] public IGpio Gpio { get; set; }
+
+        [Display("Pin Number", Order: 2)] public int PinNumber { get; set; }
+
+        [Display("Pin State", Order: 3)] public EPinState PinState { get; set; }
+
+        public override void Run()
+        {
+            try
+            {
+                Gpio.SetPinState(PinNumber, PinState);
+                UpgradeVerdict(Verdict.Pass);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                UpgradeVerdict(Verdict.Fail);
+            }
+        }
+    }
+}
