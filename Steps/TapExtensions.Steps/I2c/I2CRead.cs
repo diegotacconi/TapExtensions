63437283e9062e062b14a23f6932250b373f792f
Diego Tacconi <diego.tacconi@nokia.com>
2024-09-18 16:31:05 -0500

    Add test steps to validate I2C interface commands.

---
+++ Steps/TapExtensions.Steps/I2c/I2CRead.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OpenTap;
+using TapExtensions.Interfaces.I2c;
+
+namespace TapExtensions.Steps.I2c
+{
+    [Display("I2CRead",
+        Groups: new[] { "TapExtensions", "Steps", "I2c" })]
+    public class I2CRead : TestStep
+    {
+        [Display("I2C Adapter", Order: 1)]
+        public II2C I2CAdapter { get; set; }
+
+        [Display("Device Address", Order: 2)]
+        [Unit("Hex", StringFormat: "X2")]
+        public ushort DeviceAddress { get; set; } = 0x48;
+
+        [Display("Number of Data Bytes", Order: 4)]
+        public ushort NumberOfDataBytes { get; set; } = 2;
+
+        public override void Run()
+        {
+            try
+            {
+                I2CAdapter.Read(DeviceAddress, NumberOfDataBytes);
+                UpgradeVerdict(Verdict.Pass);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                UpgradeVerdict(Verdict.Fail);
+            }
+        }
+    }
+}
---
+++ Steps/TapExtensions.Steps/I2c/I2CWrite.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OpenTap;
+using TapExtensions.Interfaces.I2c;
+
+namespace TapExtensions.Steps.I2c
+{
+    [Display("I2CWrite",
+        Groups: new[] { "TapExtensions", "Steps", "I2c" })]
+    public class I2CWrite : TestStep
+    {
+        [Display("I2C Adapter", Order: 1)] public II2C I2CAdapter { get; set; }
+
+        [Display("Device Address", Order: 2)]
+        [Unit("Hex", StringFormat: "X2")]
+        public ushort DeviceAddress { get; set; } = 0x48;
+
+        [Display("Command", Order: 3)]
+        [Unit("Hex", StringFormat: "X2")]
+        public byte Command { get; set; } = 0x00;
+
+        public override void Run()
+        {
+            try
+            {
+                var command = new[] { Command };
+                var commandLength = Convert.ToUInt16(command.Length);
+                I2CAdapter.Write(DeviceAddress, commandLength, command);
+                UpgradeVerdict(Verdict.Pass);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                UpgradeVerdict(Verdict.Fail);
+            }
+        }
+    }
+}
