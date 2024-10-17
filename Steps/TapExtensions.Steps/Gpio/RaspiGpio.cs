65c22d07527751c82d6f9c720a477081b3260f3f
Diego Tacconi <diego@tacconi.org>
2024-10-17 17:32:56 -0500

    Add Raspi.SendSshQuery commands

--- Steps/TapExtensions.Steps/Gpio/RaspiGpio.cs
+++ Steps/TapExtensions.Steps/Gpio/RaspiGpio.cs
@@ -51,27 +51,40 @@ namespace TapExtensions.Steps.Gpio
         public void SetPinDirection(int pin, EDirection direction)
         {
             var cmd = $"sudo pinctrl set {pin} {EnumToString(direction)}";
-            Log.Debug(cmd);
+            if (!Raspi.SendSshQuery(cmd, 5, out _))
+                throw new InvalidOperationException(
+                    $"Exit status was not 0, when executing to the command of '{cmd}'");
+
+            // ToDo: verify by sending "sudo pinctrl get" and checking response
         }
 
         public void SetPinPull(int pin, EPull pull)
         {
             var cmd = $"sudo pinctrl set {pin} {EnumToString(pull)}";
-            Log.Debug(cmd);
+            if (!Raspi.SendSshQuery(cmd, 5, out _))
+                throw new InvalidOperationException(
+                    $"Exit status was not 0, when executing to the command of '{cmd}'");
+
+            // ToDo: verify by sending "sudo pinctrl get" and checking response
         }
 
         public void SetPinDrive(int pin, EDrive drive)
         {
             var cmd = $"sudo pinctrl set {pin} {EnumToString(drive)}";
-            Log.Debug(cmd);
+            if (!Raspi.SendSshQuery(cmd, 5, out _))
+                throw new InvalidOperationException(
+                    $"Exit status was not 0, when executing to the command of '{cmd}'");
+
+            // ToDo: verify by sending "sudo pinctrl get" and checking response
         }
 
         public ELevel GetPinLevel(int pin)
         {
             var cmd = $"sudo pinctrl get {pin}";
-            Log.Debug(cmd);
-            var response = " 6: ip    pu | hi // GPIO6 = input";
-            Log.Debug(response);
+            if (!Raspi.SendSshQuery(cmd, 5, out var response))
+                throw new InvalidOperationException(
+                    $"Exit status was not 0, when executing to the command of '{cmd}'");
+
             var measuredLevel = (ELevel)ParseLevel(response);
             return measuredLevel;
         }
@@ -145,231 +158,6 @@ namespace TapExtensions.Steps.Gpio
 
             return text.Substring(x, y - x);
         }
-
-        /*
-        private protected void SetPinMode(int pin, EPinInputMode mode)
-        {
-            throw new NotImplementedException();
-        }
-
-        private protected void SetPinState(int pin, EPinState state)
-        {
-            // ToDo:
-            //    /sys/class/gpio/gpio11/direction
-            //    /sys/class/gpio/gpio11/value
-            //    /dev/gpiochipN
-            //    sudo usermod -a -G gpio <username>
-
-            switch (GetPinType(state))
-            {
-                case "ip":
-                    // SendGpioInputCommand(pin, state, mode);
-                    break;
-                case "dl":
-                    SendGpioOutputCommand(pin, state);
-                    break;
-                case "dh":
-                    SendGpioOutputCommand(pin, state);
-                    break;
-            }
-        }
-
-        private protected EPinState GetPinState(int pin)
-        {
-            throw new NotImplementedException();
-        }
-
-        private protected void SendGpioInputCommand(int pin, EPinState state, EPinInputMode mode)
-        {
-            var command = "sudo raspi-gpio set " + pin + " " + GetPinType(state) + " " + GetInputMode(mode);
-            if (!Raspi.SendSshQuery(command, 5, out _))
-                throw new InvalidOperationException(
-                    $"Exit status was not 0, when executing to the command of '{command}'");
-
-            var substrings = GetGpioStatus(pin);
-
-            if (substrings[2] == "INPUT")
-            {
-                if (GetInputMode(mode) == "pd")
-                {
-                    Log.Info("pin set PULL_DOWN!");
-                }
-                else if (GetInputMode(mode) == "pn")
-                {
-                    Log.Info("pin set PULL_NONE!");
-                }
-                else if (GetInputMode(mode) == "pu")
-                {
-                    Log.Info("pin set PULL_UP!");
-                }
-                else
-                {
-                    Log.Info("verification failed!");
-                    throw new InvalidDataException(substrings[1]);
-                }
-            }
-            else
-            {
-                Log.Info("Not an input!");
-                throw new InvalidDataException(substrings[2]);
-            }
-        }
-
-        private protected void SendGpioOutputCommand(int pin, EPinState state)
-        {
-            var command = $"sudo raspi-gpio set {pin} op {GetPinType(state)}";
-            if (!Raspi.SendSshQuery(command, 5, out _))
-                throw new InvalidOperationException(
-                    $"Exit status was not 0, when executing to the command of '{command}'");
-
-            var substrings = GetGpioStatus(pin);
-
-            if (substrings[2] == "OUTPUT")
-            {
-                if (substrings[1] == "0" && GetPinType(state) == "dl")
-                {
-                    Log.Info("pin set LOW!");
-                }
-                else if (substrings[1] == "1" && GetPinType(state) == "dh")
-                {
-                    Log.Info("pin set HIGH!");
-                }
-                else
-                {
-                    Log.Info("verification failed!");
-                    throw new InvalidDataException(substrings[1]);
-                }
-            }
-            else
-            {
-                Log.Info("Not an output!");
-                throw new InvalidDataException(substrings[2]);
-            }
-        }
-
-        private protected string[] GetGpioStatus(int pin)
-        {
-                //pi@lmi:~ $ sudo raspi-gpio get
-                //BANK0 (GPIO 0 to 27):
-                //GPIO 0: level=1 func=INPUT pull=UP
-                //GPIO 1: level=1 func=INPUT pull=UP
-                //GPIO 2: level=1 alt=0 func=SDA1 pull=UP
-                //GPIO 3: level=1 alt=0 func=SCL1 pull=UP
-                //GPIO 4: level=1 func=INPUT pull=NONE
-                //GPIO 5: level=1 func=INPUT pull=UP
-                //GPIO 6: level=1 func=INPUT pull=UP
-                //GPIO 7: level=1 func=OUTPUT pull=UP
-                //GPIO 8: level=1 func=OUTPUT pull=UP
-                //GPIO 9: level=0 alt=0 func=SPI0_MISO pull=DOWN
-                //GPIO 10: level=0 alt=0 func=SPI0_MOSI pull=DOWN
-                //GPIO 11: level=0 alt=0 func=SPI0_SCLK pull=DOWN
-                //GPIO 12: level=0 func=INPUT pull=DOWN
-                //GPIO 13: level=0 func=INPUT pull=DOWN
-                //GPIO 14: level=1 alt=5 func=TXD1 pull=NONE
-                //GPIO 15: level=1 alt=5 func=RXD1 pull=UP
-                //GPIO 16: level=0 func=INPUT pull=DOWN
-                //GPIO 17: level=0 func=INPUT pull=DOWN
-                //GPIO 18: level=0 func=INPUT pull=DOWN
-                //GPIO 19: level=0 func=INPUT pull=DOWN
-                //GPIO 20: level=0 func=INPUT pull=DOWN
-                //GPIO 21: level=0 func=INPUT pull=DOWN
-                //GPIO 22: level=0 func=INPUT pull=DOWN
-                //GPIO 23: level=0 func=INPUT pull=DOWN
-                //GPIO 24: level=0 func=INPUT pull=DOWN
-                //GPIO 25: level=0 func=INPUT pull=DOWN
-                //GPIO 26: level=0 func=INPUT pull=DOWN
-                //GPIO 27: level=0 func=INPUT pull=DOWN
-                //BANK1 (GPIO 28 to 45):
-                //GPIO 28: level=1 alt=5 func=RGMII_MDIO pull=UP
-                //GPIO 29: level=0 alt=5 func=RGMII_MDC pull=DOWN
-                //GPIO 30: level=0 alt=3 func=CTS0 pull=UP
-                //GPIO 31: level=0 alt=3 func=RTS0 pull=NONE
-                //GPIO 32: level=1 alt=3 func=TXD0 pull=NONE
-                //GPIO 33: level=1 alt=3 func=RXD0 pull=UP
-                //GPIO 34: level=1 alt=3 func=SD1_CLK pull=NONE
-                //GPIO 35: level=1 alt=3 func=SD1_CMD pull=UP
-                //GPIO 36: level=1 alt=3 func=SD1_DAT0 pull=UP
-                //GPIO 37: level=1 alt=3 func=SD1_DAT1 pull=UP
-                //GPIO 38: level=1 alt=3 func=SD1_DAT2 pull=UP
-                //GPIO 39: level=1 alt=3 func=SD1_DAT3 pull=UP
-                //GPIO 40: level=0 alt=0 func=PWM1_0 pull=NONE
-                //GPIO 41: level=0 alt=0 func=PWM1_1 pull=NONE
-                //GPIO 42: level=0 func=OUTPUT pull=UP
-                //GPIO 43: level=1 func=INPUT pull=UP
-                //GPIO 44: level=1 func=INPUT pull=UP
-                //GPIO 45: level=1 func=INPUT pull=UP
-                //BANK2 (GPIO 46 to 53):
-                //GPIO 46: level=0 func=INPUT pull=UP
-                //GPIO 47: level=0 func=INPUT pull=UP
-                //GPIO 48: level=0 func=INPUT pull=DOWN
-                //GPIO 49: level=0 func=INPUT pull=DOWN
-                //GPIO 50: level=0 func=INPUT pull=DOWN
-                //GPIO 51: level=0 func=INPUT pull=DOWN
-                //GPIO 52: level=0 func=INPUT pull=DOWN
-                //GPIO 53: level=0 func=INPUT pull=DOWN
-                //pi@lmi:~ $
-
-            var command = $"sudo raspi-gpio get {pin}";
-            if (!Raspi.SendSshQuery(command, 5, out var response))
-                throw new InvalidOperationException(
-                    $"Exit status was not 0, when executing to the command of '{command}'");
-
-            var regex = new Regex("level=(\\d+)\\s+.*func=([A-Z]+)");
-            return regex.Split(response);
-        }
-
-        private protected static string GetPinType(EPinState state)
-        {
-            string pinType;
-
-            switch (state)
-            {
-                case EPinState.OutputHigh:
-                    pinType = "dh";
-                    break;
-
-                case EPinState.OutputLow:
-                    pinType = "dl";
-                    break;
-
-                case EPinState.Input:
-                    pinType = "ip";
-                    break;
-
-                default:
-                    throw new ArgumentException(
-                        $"Case not found for {nameof(state)}={state}");
-            }
-
-            return pinType;
-        }
-
-        private protected static string GetInputMode(EPinInputMode mode)
-        {
-            string inputMode;
-
-            switch (mode)
-            {
-                case EPinInputMode.PullUp:
-                    inputMode = "pu";
-                    break;
-
-                case EPinInputMode.PullDown:
-                    inputMode = "pd";
-                    break;
-
-                case EPinInputMode.PullNone:
-                    inputMode = "pn";
-                    break;
-
-                default:
-                    throw new ArgumentException(
-                        $"Case not found for {nameof(mode)}={mode}");
-            }
-
-            return inputMode;
-        }
-        */
 
         #endregion
     }
