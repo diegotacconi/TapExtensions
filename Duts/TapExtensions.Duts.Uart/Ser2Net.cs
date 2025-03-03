d01642e36f5ce0d70bebd2b3d91d996c3d6073c6
Diego Tacconi <diego@tacconi.org>
2025-03-03 13:56:21 -0600

    Implementing methods.

--- Duts/TapExtensions.Duts.Uart/Ser2Net.cs
+++ Duts/TapExtensions.Duts.Uart/Ser2Net.cs
@@ -3,6 +3,8 @@
 using System;
 using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using OpenTap;
 using TapExtensions.Interfaces.Uart;
 using TapExtensions.Shared.Telnet;
@@ -21,7 +23,16 @@ namespace TapExtensions.Duts.Uart
 
         #endregion
 
-        private static TelnetClient _client;
+        private delegate void UartEvent(string readBuffer);
+
+        private event UartEvent ReadEvent;
+        private TelnetClient _client;
+        private readonly StringBuilder _logBuffer = new StringBuilder();
+        private readonly StringBuilder _readBuffer = new StringBuilder();
+        private static bool _responseReceived;
+        private static string _expectedResponse;
+        private readonly ManualResetEvent _waitForEvent = new ManualResetEvent(false);
+        private string _response;
 
         public Ser2Net()
         {
@@ -39,54 +50,44 @@ namespace TapExtensions.Duts.Uart
         {
             base.Open();
             Connect();
-            IsConnected = false;
+        }
+
+        private void Connect()
+        {
+            // Start monitoring serial port
+            _logBuffer.Clear();
+            _readBuffer.Clear();
+
+            _client = new TelnetClient(IpAddress, TcpPort);
+            _client.Logger = Logger;
+            _client.Events.DataReceived += OnDataReceived;
+            _client.Settings.NoDelay = true;
+
+            // Number of milliseconds to wait when attempting to connect.
+            _client.Settings.ConnectTimeoutMs = 5000;
+
+            // Maximum amount of time to wait before considering the server to be idle and disconnecting from it.
+            // By default, this value is set to 0, which will never disconnect due to inactivity.
+            _client.Settings.IdleServerTimeoutMs = 0;
+
+            _client.Connect();
         }
 
         public override void Close()
         {
             Disconnect();
-            IsConnected = false;
             base.Close();
-        }
-
-        private void Connect()
-        {
-            _client = new TelnetClient(IpAddress, TcpPort);
-            _client.Events.Connected += Connected;
-            _client.Events.Disconnected += Disconnected;
-            _client.Events.DataReceived += DataReceived;
-            _client.Events.DataSent += DataSent;
-            _client.Settings.ConnectTimeoutMs = 5000;
-            _client.Settings.NoDelay = true;
-            _client.Settings.IdleServerTimeoutMs = 10000;
-            _client.Logger = Logger;
-
-            _client.Connect();
         }
 
         private void Disconnect()
         {
+            // Stop monitoring serial port
+            _client.Events.DataReceived -= OnDataReceived;
+            _logBuffer.Clear();
+            _readBuffer.Clear();
+
+            // Close serial port
             _client.Disconnect();
-        }
-
-        private void Connected(object sender, ConnectionEventArgs e)
-        {
-            Log.Debug("*** Server " + e.IpPort + " connected");
-        }
-
-        private void Disconnected(object sender, ConnectionEventArgs e)
-        {
-            Log.Debug("*** Server " + e.IpPort + " disconnected");
-        }
-
-        private void DataReceived(object sender, DataReceivedEventArgs e)
-        {
-            Log.Debug("[" + e.IpPort + "] " + Encoding.UTF8.GetString(e.Data.Array, 0, e.Data.Count));
-        }
-
-        private void DataSent(object sender, DataSentEventArgs e)
-        {
-            Log.Debug("[" + e.IpPort + "] sent " + e.BytesSent + " bytes");
         }
 
         private void Logger(string msg)
@@ -94,19 +95,111 @@ namespace TapExtensions.Duts.Uart
             Log.Debug(msg);
         }
 
+        private void OnDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data.Array == null)
+                return;
+
+            var data = Encoding.UTF8.GetString(e.Data.Array, 0, e.Data.Count);
+            _readBuffer.Append(data);
+            ReadEvent?.Invoke(_readBuffer.ToString());
+            LogLineByLine(data, e.IpPort);
+        }
+
         public bool Expect(string expectedResponse, int timeout)
         {
-            throw new NotImplementedException();
+            // Start monitoring serial port
+            _response = string.Empty;
+            _responseReceived = false;
+            _expectedResponse = expectedResponse;
+            ReadEvent += OnReadEvent;
+
+            // Wait for serial port to receive expected response
+            var waitEnded = _waitForEvent.WaitOne(timeout * 1000);
+            if (!waitEnded)
+                Log.Warning("Serial port timed-out!");
+
+            // Stop monitoring serial port
+            ReadEvent -= OnReadEvent;
+            _waitForEvent.Reset();
+
+            return _responseReceived && waitEnded;
+        }
+
+        private void OnReadEvent(string readBuffer)
+        {
+            if (readBuffer.Contains(_expectedResponse))
+            {
+                _response = readBuffer;
+                _responseReceived = true;
+                _readBuffer.Clear();
+                _waitForEvent.Set();
+            }
         }
 
         public string Query(string command, string expectedEndOfMessage, int timeout)
         {
-            throw new NotImplementedException();
+            _readBuffer.Clear();
+            Write(command);
+            Expect(expectedEndOfMessage, timeout);
+            var response = _response;
+
+            // ToDo: remove debug lines below
+            // var msg = Regex.Replace(response, @"\t|\n|\r", "_");
+            // Log.Debug($"RESPONSE: {msg}");
+
+            return response;
         }
 
         public void Write(string command)
         {
-            throw new NotImplementedException();
+            // ToDo: remove debug line below
+            // Log.Debug($"{_client.ServerAddress} >> {command}");
+
+            _client.Send(command + "\n");
+        }
+
+        private void LogLineByLine(string data, string tag)
+        {
+            foreach (var c in data)
+            {
+                _logBuffer.Append(c);
+
+                // Show one line per log message
+                if (c == '\n' || c == '\r')
+                {
+                    var currentLine = _logBuffer.ToString();
+                    _logBuffer.Clear();
+
+                    // Split into lines
+                    var lines = currentLine.Split(new[] { "\r\n", "\n\r", "\r", "\n" },
+                        StringSplitOptions.RemoveEmptyEntries);
+
+                    foreach (var line in lines)
+                    {
+                        // Go to the next foreach line, if string is empty
+                        if (string.IsNullOrWhiteSpace(line))
+                            continue;
+
+                        // Remove ANSI escape codes from log message
+                        var lineWithoutAnsiEscapeCodes =
+                            Regex.Replace(line, @"\x1B\[[^@-~]*[@-~]", "", RegexOptions.Compiled);
+
+                        // Go to the next foreach line, if string is empty
+                        if (string.IsNullOrWhiteSpace(lineWithoutAnsiEscapeCodes))
+                            continue;
+
+                        var msg = $"{tag} << {lineWithoutAnsiEscapeCodes}";
+
+                        // Truncate log message to a maximum sting length
+                        const int maxLength = 500;
+                        if (msg.Length > maxLength)
+                            msg = msg.Substring(0, maxLength) + "***";
+
+                        Log.Debug(msg);
+                    }
+                }
+            }
         }
     }
 }
