e31349a6b7e4c291f5b2eae59ce9b0aaafd75a0b
Diego Tacconi <diego@tacconi.org>
2025-06-12 17:36:28 -0500

    Add ElapsedTime test step.

---
+++ Steps/TapExtensions.Steps/Benchmark/ElapsedTime .cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OpenTap;
+
+namespace TapExtensions.Steps.Benchmark
+{
+    [Display("ElapsedTime",
+        Groups: new[] { "TapExtensions", "Steps", "Benchmark" })]
+    public class ElapsedTime : TestStep
+    {
+        public override void Run()
+        {
+            var startTime = PlanRun.StartTime;
+            Log.Debug($"startTime = {startTime}");
+
+            var elapsedTime = DateTime.Now - startTime;
+            Log.Debug($"elapsedTime = {elapsedTime}");
+        }
+    }
+}
