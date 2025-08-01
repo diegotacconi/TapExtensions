3436ce98ee863fdd350fa8665e571ddae35a565a
Diego Tacconi <diego@tacconi.org>
2025-08-01 15:12:47 -0500

    Add ParentChild for temperature measurements and calculate its average.

---
+++ Steps/TapExtensions.Steps/ParentChild/Temperature.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Xml.Serialization;
+using OpenTap;
+
+namespace TapExtensions.Steps.ParentChild
+{
+    [Display("Temperature",
+        Groups: new[] { "TapExtensions", "Steps", "ParentChild" })]
+    [AllowChildrenOfType(typeof(TemperatureAverage))]
+    public class Temperature : TestStep
+    {
+        [XmlIgnore]
+        [Browsable(false)]
+        public List<double> Measurements { get; } = new List<double>();
+
+        public override void Run()
+        {
+            try
+            {
+                Measurements.Clear();
+
+                var random = new Random();
+                for (var i = 1; i <= 5; i++)
+                {
+                    var value = Math.Round(random.NextDouble() * 5, 3);
+                    Measurements.Add(value);
+                }
+
+                foreach (var m in Measurements)
+                    Log.Debug($"{m}");
+
+                RunChildSteps();
+
+                UpgradeVerdict(Verdict.Pass);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                UpgradeVerdict(Verdict.Fail);
+            }
+        }
+    }
+}
---
+++ Steps/TapExtensions.Steps/ParentChild/TemperatureAverage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml.Serialization;
+using OpenTap;
+
+namespace TapExtensions.Steps.ParentChild
+{
+    [Display("TemperatureAverage",
+        Groups: new[] { "TapExtensions", "Steps", "ParentChild" })]
+    [AllowAsChildIn(typeof(Temperature))]
+    public class TemperatureAverage : TestStep
+    {
+        [XmlIgnore]
+        [Browsable(true)]
+        [Display("Measurements", Group: "From Parent", Collapsed: true)]
+        public List<double> Measurements => GetParent<Temperature>().Measurements;
+
+        public override void Run()
+        {
+            try
+            {
+                foreach (var m in Measurements)
+                    Log.Debug($"{m}");
+
+                var average = Measurements.Average();
+                Log.Debug($"average = {average}");
+
+                UpgradeVerdict(Verdict.Pass);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                UpgradeVerdict(Verdict.Fail);
+            }
+        }
+    }
+}
