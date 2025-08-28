21a28fd7fa4d6251d7e744336017a0954d8e13cc
Diego Tacconi <diego@tacconi.org>
2025-08-28 17:18:42 -0500

    Add ChildUpdatingResources

---
+++ Steps/TapExtensions.Steps/ParentChild/ChildUpdatingResources.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Xml.Serialization;
+using OpenTap;
+
+namespace TapExtensions.Steps.ParentChild
+{
+    [Display("ChildUpdatingResources",
+        Groups: new[] { "TapExtensions", "Steps", "ParentChild" })]
+    [AllowAsChildIn(typeof(ParentWithResources))]
+    public class ChildUpdatingResources : TestStep
+    {
+        [XmlIgnore]
+        [Browsable(false)]
+        public string SomeString
+        {
+            get => GetParent<ParentWithResources>().SomeString;
+            set => GetParent<ParentWithResources>().SomeString = value;
+        }
+
+        [Display("AddToSomeString", Order: 3)] public string AddToSomeString { get; set; } = "DEF";
+
+        public override void Run()
+        {
+            try
+            {
+                Log.Debug($"Child={Name}(before), String={SomeString}.");
+
+                SomeString += AddToSomeString;
+
+                Log.Debug($"Child={Name}(before), String={SomeString}.");
+
+                UpgradeVerdict(Verdict.Pass);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                UpgradeVerdict(Verdict.Fail);
+            }
+        }
+    }
+}
