0cb439270e45024b7ee9b3c6483efb010659bbe4
Diego Tacconi <diego@tacconi.org>
2025-12-07 12:18:27 +0200

    Add basic IDio interface.

---
+++ Interfaces/TapExtensions.Interfaces/Dio/IDio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using OpenTap;
+
+namespace TapExtensions.Interfaces.Dio
+{
+    public interface IDio : IInstrument
+    {
+        List<EInputState> GetInputState(List<short> channels);
+
+        void SetOutputState(List<short> channels, List<EOutputState> states);
+    }
+
+    #region Enums
+
+    public enum EInputState
+    {
+        Low = 0,
+        High = 1,
+        Middle = 3
+    }
+
+    public enum EOutputState
+    {
+        Sink,
+        Source,
+        Off
+    }
+
+    #endregion
+}
